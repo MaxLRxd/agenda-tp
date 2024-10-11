bb27f1ce6954a1ee52d95d27249ef58dcf026b19
Maxi <maximorojas002@gmail.com>
2024-10-11 18:18:34 -0300

    correcto funcionamiento de agregarTelefono() y su sobrecarga

--- Contactos.java
+++ Contactos.java
@@ -81,35 +81,44 @@ public class Contactos{
     }
     
     public void agregarTelefono(String caracteristica, int numero){
-        if(caracteristica.length() > 0){
-            if(String.valueOf(numero).length() > 0){
-                
+        if(!validarCaracteristica(caracteristica)){
+            if(!validarNumero(numero)){
+                if(elementoVacio() != -1){
+                    telefono[elementoVacio()] = caracteristica+"-"+numero;
+                }else{
+                    System.out.println("No existen elementos vacíos");
+                }
+            }else{
+                System.out.println("El numero ingresado es erroneo");
             }
+        }else{
+            System.out.println("El numero ingresado es erroneo");
         }
     }
     
+
      public void agregarTelefono(){
-            System.out.println("Ingrese caracterstica telefónica");
-            String caracteristica = sc.nextLine();
-            if(!validarCaracteristica(caracteristica)){
-                do{
-                    System.out.println("¡Caracteristica no valida!");
-                    caracteristica = sc.nextLine();
-                }while(!validarCaracteristica(caracteristica));
-            }
-            System.out.println("Ingrese numero telefonico");
-            int numero = sc.nextInt();
-            if(!validarNumero(numero)){
-                do{
-                    System.out.println("¡numero no valido!");
-                    numero = sc.nextInt();
-                }while(!validarNumero(numero));
-            }
-            for (int i = 0; i < 5; i++) {
-                if (telefono[i] == null || telefono[i].isEmpty()) {
-                    telefono[i] = caracteristica+"-"+numero;
-                    break;
+            if(elementoVacio() != -1){
+                System.out.println("Ingrese caracterstica telefónica");
+                String caracteristica = sc.nextLine();
+                if(!validarCaracteristica(caracteristica)){
+                    do{
+                        System.out.println("¡Caracteristica no valida!");
+                        caracteristica = sc.nextLine();
+                    }while(!validarCaracteristica(caracteristica));
                 }
+                System.out.println("Ingrese numero telefonico");
+                int numero = sc.nextInt();
+                if(!validarNumero(numero)){
+                    do{
+                        System.out.println("¡numero no valido!");
+                        numero = sc.nextInt();
+                    }while(!validarNumero(numero));
+                }
+                telefono[elementoVacio()] = caracteristica+"-"+numero;
+            }
+            else{
+                System.out.println("No existe más lugar para agregar números telefónicos");
             }
     }
 }
